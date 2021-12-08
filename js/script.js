13ae9b44e2d89515a2840b752ddefcfebb7cfb55
eduardalucas <89199275+eduardalucas@users.noreply.github.com>
2021-12-08 08:15:25 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ function carregar(){
     msg.innerHTML=`Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         img.src = 'images/manhã.png'
-        document.body.style.background = '#d9f5ff'
+        document.body.style.background = '#ffe4d9'
     } else if (hora >= 12 && hora< 18){
         img.src = 'images/tarde.png'
-        document.body.style.background = '#ffe4d9'
+        document.body.style.background = '#d9f5ff'
     } else{
         img.src = 'images/noite.png'
         document.body.style.background = ' #8d7bab'
