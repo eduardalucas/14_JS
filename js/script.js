173811e9b261a0d1132042b8f5918c17afac61db
eduardalucas <89199275+eduardalucas@users.noreply.github.com>
2021-12-07 21:15:26 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML=`Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'images/manhã.png'
+        document.body.style.background = '#d9f5ff'
+    } else if (hora >= 12 && hora< 18){
+        img.src = 'images/tarde.png'
+        document.body.style.background = '#ffe4d9'
+    } else{
+        img.src = 'images/noite.png'
+        document.body.style.background = ' #8d7bab'
+    }
+    }
---
+++ js/script2.js
@@ -0,0 +1,49 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if (fano.value.length == 0||fano.value > ano){
+        window.alert ('[ERRO] Verifique os dados e tente novamente!')
+    } else if(fano.value <0){
+        window.alert ('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade<0){
+                window.alert ('[ERRO] Verifique os dados e tente novamente!')
+            } if (idade>= 0 && idade <= 10){
+                img.setAttribute ('src', 'images/lboy.png')
+            } else if ( idade< 20){
+                img.setAttribute ('src', 'images/boy.png')
+            } else if(idade<50){
+                img.setAttribute ('src', 'images/man.png')
+            } else {
+                img.setAttribute ('src', 'images/grampa.png')
+            }
+        }  
+
+        else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade>= 0 && idade <= 10){
+                img.setAttribute ('src', 'images/lgirl.png')
+            } else if ( idade< 20){
+                img.setAttribute ('src', 'images/girl.png')
+            } else if(idade<50){
+                img.setAttribute ('src', 'images/woman.png')
+            } else {
+                img.setAttribute ('src', 'images/gramma.png')
+            }
+        }
+
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
